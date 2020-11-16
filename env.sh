ecdea36d6b399f04fd904c2ff31b7ae32951ec78
Min Wei <min.mwei@gmail.com>
2020-11-15 19:56:40 -0800

    fix path

--- env.sh
+++ env.sh
@@ -1,2 +1,2 @@
 export RADIXDB_SOURCE=~/RadixDB
-export PATH=$RADIXDB_SOURCE/bin:$RADIXDB_SOURCE/db/dist/pg/bin:/usr/local/bin:$PATH
+export PATH=$RADIXDB_SOURCE/bin:$RADIXDB_SOURCE/dist/pg/bin:/usr/local/bin:$PATH
